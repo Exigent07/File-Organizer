e397c615dd0bb0989320531fb77d47c0e1766437
Exigent07 <aravindhlearner07@gmail.com>
2023-11-24 15:24:29 +0530

    Wrote some functions

--- main.py
+++ main.py
@@ -1,13 +1,46 @@
 import os
 
-path = os.getcwd()
-filesInFolder = []
+# Declaring functions
 
-for item in (os.listdir(path + "/TestFolder")):
-    currentFile = item
-    if (currentFile[:1] == "."):
-        continue
-    else:
-        filesInFolder.append(currentFile)
+# Function for creating a Directory
+def makeDIR(name, parentDIR):
+    path = os.path.join(parentDIR, name)
+    os.mkdir(path)
 
-print(filesInFolder)
+# Function for finding Files
+def findFiles(path):
+    filesInFolder = []
+
+    for item in (os.listdir(path)):
+        currentFile = item
+        if (currentFile[:1] == "."):
+            continue
+        else:
+            filesInFolder.append(currentFile)
+
+    return filesInFolder
+
+# Function to find the types of files and their count
+def findType(files):
+    typesOfExt = {}
+    for current in files:
+        currentFile = list(current)
+        indexOfHidden = currentFile.index(".")
+        currentExt = "".join(currentFile[char] for char in range(indexOfHidden + 1, len(currentFile)))
+        try:
+            if typesOfExt[currentExt]:
+                typesOfExt[currentExt].append(current)
+        except:
+            typesOfExt[currentExt] = [current]
+
+    return typesOfExt
+
+# Testing 
+path = os.getcwd() + "/TestFolder"
+
+files = findFiles(path)
+typeCount = findType(files)
+
+print("Files:", typeCount)
+
+# makeDIR("Test", path)
