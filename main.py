16209889c4a573bc8cec257f92ecb17e5028c777
Exigent07 <aravindhlearner07@gmail.com>
2023-11-24 16:13:16 +0530

    Completed base function

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 import os
+import shutil
 
 # Declaring functions
 
 # Function for creating a Directory
 def makeDIR(name, parentDIR):
-    path = os.path.join(parentDIR, name)
-    os.mkdir(path)
+    try:
+        path = os.path.join(parentDIR, name)
+        os.mkdir(path)
+    except:
+        print("Folder Already Exists, so skipping!")
 
 # Function for finding Files
 def findFiles(path):
@@ -35,12 +39,58 @@ def findType(files):
 
     return typesOfExt
 
+def creatAndMove(path, filesTypes):
+    fileExtensions = {
+    ".txt": "Text Files",
+    ".doc": "Word Documents",
+    ".docx": "Word Documents",
+    ".xls": "Excel Spreadsheets",
+    ".xlsx": "Excel Spreadsheets",
+    ".ppt": "PowerPoint Presentations",
+    ".pptx": "PowerPoint Presentations",
+    ".pdf": "PDF Files",
+    ".jpg": "JPEG Image Files",
+    ".png": "PNG Image Files",
+    ".gif": "GIF Image Files",
+    ".mp3": "MP3 Audio Files",
+    ".mp4": "MP4 Video Files",
+    ".avi": "AVI Video Files",
+    ".exe": "Executable Files",
+    ".zip": "ZIP Archives",
+    ".rar": "RAR Archives",
+    ".html": "HTML Files",
+    ".css": "CSS Stylesheets",
+    ".js": "JavaScript Files",
+    ".py": "Python Scripts",
+    ".java": "Java Source Files",
+    ".cpp": "C++ Source Files",
+    ".c": "C Source Files",
+}
+    types = []
+    dirNames = []
+
+    for key in filesTypes.keys():
+        types.append(key)
+    for ext in types:
+        extension = "." + ext
+        if extension in fileExtensions.keys():
+            dirNames.append(fileExtensions[extension])
+    for name in dirNames:
+        makeDIR(name, path)
+        CurrentExt = list(fileExtensions.keys())[list(fileExtensions.values()).index(name)][1:]
+        files = filesTypes[CurrentExt]
+        for moveFiles in files:
+            shutil.move((path + "/" + moveFiles), (path + "/" + name + "/" + moveFiles))
+    return True
 # Testing 
 path = os.getcwd() + "/TestFolder"
 
 files = findFiles(path)
 typeCount = findType(files)
 
-print("Files:", typeCount)
+isWorkDone = creatAndMove(path, typeCount)
 
-# makeDIR("Test", path)
+if isWorkDone:
+    print("Organizing Successful!!")
+else:
+    print("Something Went Wrong XD")
